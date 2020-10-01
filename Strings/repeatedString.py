35b41d2bbf5f3d75141ed980c353004fe42adce1
Derek Dyer <derekdyer02@gmail.com>
2020-10-01 14:21:20 -0700

    adds repeatedString.py

--- Strings/repeatedString.py
+++ Strings/repeatedString.py
@@ -3,25 +3,26 @@ import math
 def repeatedString(s, n):
     #Edge case: string contains only 1 character
     if len(s) == 1:
+        #if there are no 'a's
         if 'a' not in s:
             return 0
+        #if n == 0
+        if n == 0:
+            return 0
         return n
-    #string needs to be at least one character and n needs to be at least one character
+    #minimun constraint for s and n
     min_n = 1
-    #string must be <= 100 characters
+    #maximum constraint for string
     maxs = 100
-    #max number is 10 ^^ 12
+    #maximum constraint for n
     max_n = math.pow(10, 12)
-    #length for if statement logic comparison of the above
+    #string length
     str_len = len(s)
-    #a list of each character in lowercase  as long as we don't exceed the max string length
-    seq = [c.lower() for c  in s * n if len(s) < maxs and len(s) > min_n and n < max_n]
-    #counts the number of times 'a' appears in our string
-    seq_len = seq[:n].count('a')
-    #if the constraints are met
+    #lower all characters and store each string char in list
+
     if type(s) == str and str_len >= min_n and str_len <= maxs and n == int(n) and n >= min_n and n <= max_n:
-        #return the remainder of our sequence length and n
-        return seq_len % n 
+        return s.count("a") * (n // len(s)) + s[:n % len(s)].count("a")
 
 
-print(repeatedString("AbA", 10))
+print(repeatedString("kmretasscityylpdhuwjirnqimlkcgxubxmsxpypgzxtenweirknjtasxtvxemtwxuarabssvqdnktqadhyktagjxoanknhgilnm"
+, 736778906400))
